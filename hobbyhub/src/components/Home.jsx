1f08d3e3a799b2d8d330da73cd2198c919c7677a
DiegoFNav <diegonavarro12703@gmail.com>
2024-04-20 22:05:28 -0700

    Added Search bar to home page

--- hobbyhub/src/components/Home.jsx
+++ hobbyhub/src/components/Home.jsx
@@ -1,29 +1,30 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 
 function Home() {
   const navigate = useNavigate();
   const supabase = useSupabaseClient();
   const [posts, setPosts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
 
   useEffect(() => {
-    async function fetchPosts() {
-      try {
-        const { data, error } = await supabase.from('posts').select('*');
-        if (error) {
-          console.error('Error fetching posts:', error.message);
-        } else {
-          setPosts(data);
-        }
-      } catch (error) {
-        console.error('Error fetching posts:', error.message);
-      }
-    }
-
     fetchPosts();
   }, []);
+
+  const fetchPosts = async () => {
+    try {
+      const { data, error } = await supabase.from('posts').select('*');
+      if (error) {
+        console.error('Error fetching posts:', error.message);
+      } else {
+        setPosts(data);
+      }
+    } catch (error) {
+      console.error('Error fetching posts:', error.message);
+    }
+  };
 
   const handleCardClick = (id) => {
     navigate(`/post/${id}`);
@@ -63,6 +64,14 @@ function Home() {
     }
   };
 
+  const handleSearchChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const filteredPosts = posts.filter(post =>
+    post.title.toLowerCase().includes(searchQuery.toLowerCase())
+  );
+
   return (
     <div className="home_container">
       <div className="sorting_buttons">
@@ -70,8 +79,18 @@ function Home() {
         <Button variant="primary" onClick={handleSortByNewest}>Newest</Button>
         <Button variant="primary" onClick={handleSortByMostPopular}>Most Popular</Button>
       </div>
-        {posts.map(post => (
-          <div
+      <Form className="search_bar">
+        <Form.Group controlId="searchQuery">
+          <Form.Control
+            type="text"
+            placeholder="Search posts by title"
+            value={searchQuery}
+            onChange={handleSearchChange}
+          />
+        </Form.Group>
+      </Form>
+      {filteredPosts.map(post => (
+        <div
           key={post.id}
           className="home_card"
           onClick={() => handleCardClick(post.id)}
@@ -84,8 +103,7 @@ function Home() {
           <h2 className="card_title">{post.title}</h2>
           <h6>{post.upvotes} upvotes</h6>
         </div>
-        ))}
-        
+      ))}
     </div>
   );
 }
